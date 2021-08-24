ec615e4e3bb6d65d12ee3b15d00f2c00f6f4ebca
Venkata Aravind Singaraju <venkataaravind.singa@hcl.com>
2021-08-24 10:43:45 +0530

    class.py is added

---
+++ class.py
@@ -0,0 +1,17 @@
+class Human:
+    def __init__(self,name,occupation):
+        self.name=name
+        self.occupation=occupation
+    def do_work(self):
+        if self.occupation=="actor":
+            print(self.name,"acting in films")
+        elif self.occupation=="tennis player":
+            print(self.name,"plays tennis")
+    def speaks(self):
+        print(self.name,"says hi")
+tom=Human("Tom Cruise","actor")
+tom.do_work()
+tom.speaks()
+maria=Human("Maria Sharapova","tennis player")
+maria.do_work()
+maria.speaks()
