79bf72a5c158772461c495c4cb121e04a6381c11
AravindSv15 <aravindsv01@gmail.com>
2021-09-02 09:48:46 +0530

    areas program commited

---
+++ area.py
@@ -0,0 +1,17 @@
+#base=10
+#height=4
+#area=1/2*(base*height)
+#print(area)
+
+
+
+#def cal_area(base, height):
+ #   print("__name__:",__name__)
+ #   return 1 / 2 * (base * height)
+#if __name__=="__main__":
+ #   a=cal_area(10,30)
+ #   print("area:",a)
+
+
+city="los angels"
+city.find("x")
---
+++ areacaller.py
@@ -0,0 +1,4 @@
+import area
+print("im in caller")
+a=area.cal_area(2,5)
+print(a)
